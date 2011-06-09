545f13d83cf5e764935f6601d91d8e8f0e847b78
Geoff Hackett <ghackett@gmail.com>
2011-06-09 12:09:35 -0400

    added a HandyFrameLayout with an OnSizeChangedListener

---
+++ src/org/droidkit/widget/HandyFrameLayout.java
@@ -0,0 +1,40 @@
+package org.droidkit.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class HandyFrameLayout extends FrameLayout {
+    
+    public interface OnSizeChangedListener {
+        public void onSizeChanged(HandyFrameLayout frameLayout, int w, int h, int oldw, int oldh);
+    }
+    
+    private OnSizeChangedListener mSizeListener;
+
+    public HandyFrameLayout(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+    public HandyFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+    public HandyFrameLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setOnSizeChangedListener(OnSizeChangedListener listener) {
+        mSizeListener = listener;
+    }
+    
+    @Override
+    public void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mSizeListener != null)
+            mSizeListener.onSizeChanged(this, w, h, oldw, oldh);
+    }
+}
