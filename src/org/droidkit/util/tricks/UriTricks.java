6e3f0f87600a67dba7ce82bc60582d67a8a93a8b
Geoff Hackett <ghackett@gmail.com>
2011-07-06 12:59:40 -0400

    added UriTricks to strip query and fragment from uris

---
+++ src/org/droidkit/util/tricks/UriTricks.java
@@ -0,0 +1,15 @@
+package org.droidkit.util.tricks;
+
+import android.net.Uri;
+
+public class UriTricks {
+    
+    public static String stripQueryAndFragments(Uri baseUri) {
+        String uriStr = baseUri.toString();
+        int index = uriStr.indexOf("?");
+        if (index >= 0)
+            uriStr = uriStr.substring(0, index);
+        return uriStr;
+    }
+
+}
