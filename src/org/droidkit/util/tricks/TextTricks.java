7fef018c6626e8353f26aec6f1a0af9e30a274be
Geoff Hackett <ghackett@gmail.com>
2011-07-08 13:48:17 -0400

    added isValidEmail and isValidUSAPhoneNumber regex checks to TextTricks

--- src/org/droidkit/util/tricks/TextTricks.java
+++ src/org/droidkit/util/tricks/TextTricks.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class TextTricks {
     public static String joinWithPrefix(String prefix, String[] list, String separator) {
@@ -62,5 +63,16 @@ public class TextTricks {
         while (hash.length() < 32)
             hash = "0" + hash;
         return hash;
+    }
+    
+    public static final Pattern REGEXP_PHONE_NUMBER_USA = Pattern.compile("^\\+?1?[- ]?\\(?\\d{3}\\)?[- ]?\\d{3}[- ]?\\d{4}$");
+    public static final Pattern REGEXP_SIMPLE_EMAIL = Pattern.compile("^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,8}$");
+
+    public static boolean isValidUSAPhoneNumber(String phoneNumber) {
+        return REGEXP_PHONE_NUMBER_USA.matcher(phoneNumber).matches();
+    }
+    
+    public static boolean isValidEmail(String email) {
+        return REGEXP_SIMPLE_EMAIL.matcher(email).matches();
     }
 }
