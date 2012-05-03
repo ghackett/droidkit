c0d53dd0d58972698f345349fed20d9ca7a9efcd
Geoff Hackett <ghackett@gmail.com>
2012-05-03 16:38:28 -0400

    added a couple copyright and author comments

--- src/org/droidkit/widget/PinchImageView.java
+++ src/org/droidkit/widget/PinchImageView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2012 GroupMe, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.droidkit.widget;
 
 
@@ -18,6 +33,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
+/**
+ * 
+ * @author Geoff Hackett
+ *
+ */
 public class PinchImageView extends View implements OnScaleGestureListener, OnGestureListener, OnDoubleTapListener, OnTouchListener  {
 //	private static final String TAG = "PinchImageView";
 	
