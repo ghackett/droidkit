50df2dce09a77eca34412fa8c9a99a69d0bcd4b0
Geoff Hackett <ghackett@gmail.com>
2012-08-03 10:37:47 -0400

    custom drawing in FullBleedImageView only works properly when image is a raw bitmap and scaleType is CenterCrop, otherwise fall back to default drawing

--- src/org/droidkit/widget/FullBleedImageView.java
+++ src/org/droidkit/widget/FullBleedImageView.java
@@ -8,10 +8,10 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
-
 public class FullBleedImageView extends HandyImageView {
-	
+
 	private Rect mSrcRect = null, mDestRect = null;
+	private boolean mCustomDraw = false;
 
 	public FullBleedImageView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
@@ -27,87 +27,101 @@ public class FullBleedImageView extends HandyImageView {
 		super(context);
 		initFullBleedImageView();
 	}
-	
+
 	private void initFullBleedImageView() {
 		setScaleType(ScaleType.CENTER_CROP);
 	}
-	
-	   @Override
-		protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-			super.onLayout(changed, left, top, right, bottom);
-			if (changed) {
-				mSrcRect = null;
-				mDestRect = null;
-				postInvalidate();
-			}
-		}
 
-		@Override
-		public void setImageDrawable(Drawable drawable) {
-			super.setImageDrawable(drawable);
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+		if (changed) {
 			mSrcRect = null;
+			mDestRect = null;
 			postInvalidate();
 		}
+	}
+	
+	
 
-		private Rect getSourceRect(Bitmap b) {
-	    	if (mSrcRect != null)
-	    		return mSrcRect;
-	    	
-	    	final int vWidth = getWidth();
-	    	if (vWidth > 0) {
-				final int vHeight = getHeight();
-				final int bWidth = b.getWidth();
-				final int bHeight = b.getHeight();
-				final float vWidthOverHeight = (float)vWidth/(float)vHeight;
-				int srcWidth = (int)((float)bHeight * vWidthOverHeight);
-				int srcHeight = 0;
-				if (srcWidth <= bWidth) {
-					srcHeight = bHeight;
-				} else {
-					final float vHeightOverWidth = (float)vHeight/(float)vWidth;
-					srcHeight = (int)((float)bWidth * vHeightOverWidth);
-					srcWidth = bWidth;
-				}
-				final int srcLeft = (bWidth - srcWidth)/2;
-				final int srcRight = srcLeft + srcWidth;
-				final int srcTop = (bHeight - srcHeight)/2;
-				final int srcBottom = srcTop + srcHeight;
-				mSrcRect = new Rect(srcLeft, srcTop, srcRight, srcBottom);
-	    	}
-	    	return mSrcRect;
-	    }
-		
-		private Rect getDestRect() {
-			if (mDestRect != null)
-				return mDestRect;
-			final int width = getWidth();
-			if (width > 0) {
-				final int height = getHeight();
-				mDestRect = new Rect(0, 0, width, height);
+	@Override
+	public void setImageBitmap(Bitmap bm) {
+		mCustomDraw = true;
+		super.setImageBitmap(bm);
+	}
+
+	@Override
+	public void setImageResource(int resId) {
+		mCustomDraw = false;
+		super.setImageResource(resId);
+	}
+
+	@Override
+	public void setImageDrawable(Drawable drawable) {
+		super.setImageDrawable(drawable);
+		mSrcRect = null;
+		postInvalidate();
+	}
+
+	private Rect getSourceRect(Bitmap b) {
+		if (mSrcRect != null)
+			return mSrcRect;
+
+		final int vWidth = getWidth();
+		if (vWidth > 0) {
+			final int vHeight = getHeight();
+			final int bWidth = b.getWidth();
+			final int bHeight = b.getHeight();
+			final float vWidthOverHeight = (float) vWidth / (float) vHeight;
+			int srcWidth = (int) ((float) bHeight * vWidthOverHeight);
+			int srcHeight = 0;
+			if (srcWidth <= bWidth) {
+				srcHeight = bHeight;
+			} else {
+				final float vHeightOverWidth = (float) vHeight / (float) vWidth;
+				srcHeight = (int) ((float) bWidth * vHeightOverWidth);
+				srcWidth = bWidth;
 			}
+			final int srcLeft = (bWidth - srcWidth) / 2;
+			final int srcRight = srcLeft + srcWidth;
+			final int srcTop = (bHeight - srcHeight) / 2;
+			final int srcBottom = srcTop + srcHeight;
+			mSrcRect = new Rect(srcLeft, srcTop, srcRight, srcBottom);
+		}
+		return mSrcRect;
+	}
+
+	private Rect getDestRect() {
+		if (mDestRect != null)
 			return mDestRect;
+		final int width = getWidth();
+		if (width > 0) {
+			final int height = getHeight();
+			mDestRect = new Rect(0, 0, width, height);
+		}
+		return mDestRect;
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		if (mCustomDraw && getScaleType() == ScaleType.CENTER_CROP) {
+			final Drawable d = getDrawable();
+			if (d instanceof BitmapDrawable) {
+				Bitmap b = ((BitmapDrawable) d).getBitmap();
+				if (b != null && !b.isRecycled()) {
+					final Rect srcRect = getSourceRect(b);
+					if (srcRect != null) {
+						final Rect destRect = getDestRect();
+						if (destRect != null) {
+							canvas.drawBitmap(b, srcRect, destRect, null);
+							return;
+						}
+					}
+				}
+			}
 		}
 
-		@Override
-		protected void onDraw(Canvas canvas) {
-	    	final Drawable d = getDrawable();
-	    	if (d instanceof BitmapDrawable) {
-	    		Bitmap b = ((BitmapDrawable) d).getBitmap();
-	    		if (b != null && !b.isRecycled()) {
-		    		final Rect srcRect = getSourceRect(b);
-		    		if (srcRect != null) {
-		    			final Rect destRect = getDestRect();
-		    			if (destRect != null) {
-		    				canvas.drawBitmap(b, srcRect, destRect, null);
-		    				return;
-		    			}
-		    		}
-	    		}
-	    	}
-	    	
-			super.onDraw(canvas);
-		}
-
-		
+		super.onDraw(canvas);
+	}
 
 }
