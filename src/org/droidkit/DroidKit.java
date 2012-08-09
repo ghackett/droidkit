c900fd7449d7aa02b3a6179462c226f3d34c5c02
Geoff Hackett <ghackett@gmail.com>
2012-08-09 11:01:08 -0400

    added DroidKit.isGingerbread, cause we needed that for some reason

--- src/org/droidkit/DroidKit.java
+++ src/org/droidkit/DroidKit.java
@@ -365,6 +365,12 @@ public class DroidKit {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
     }
     
+    public static boolean isGingerbread() {
+        // Can use static final constants like GINGERBREAD, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
+    }
+    
     public static boolean isHoneycomb() {
         // Can use static final constants like HONEYCOMB, declared in later versions
         // of the OS since they are inlined at compile time. This is guaranteed behavior.
